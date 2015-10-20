1ed7322d72dd696a349922574fc0acb10bd81695
Wildhoney <adam.timberlake@gmail.com>
2015-10-20 08:54:48 +0100

    Setup example with setState

--- example/Gremlin.js
+++ example/Gremlin.js
@@ -1,22 +1,39 @@
 /**
  * @method findHuman
- * @return {Boolean}
+ * @return {Object|Boolean}
  */
 export const findHuman = () => {
-    return Math.random() > 0.5;
+    const human = { name: 'Adam', age: 30 };
+    return (Math.random() > 0.5) ? { human } : false;
+};
+
+/**
+ * @method eatBrain
+ * @param {Object} human
+ * @return {void}
+ */
+export const eatBrain = human => {
+
+    if (human) {
+        console.log(`Eating brain of ${human.name}. Nom, nom, nom!`);
+    }
+
 };
 
 /**
  * @method render
  * @param {Object} props
+ * @param {Object} state
+ * @param {Function} setState
  * @return {XML}
  */
-export function render({ props }) => {
+export function render({ props, state, setState }) => {
 
     return (
         <article>
             <h1>{props.name}</h1>
-            <button onClick={() => findHuman()} />
+            <button onClick={() => setState(findHuman())}>Find Human</button>
+            <button onClick={() => eatBrain(state.human)}>Eat Brain</button>
         </article>
     );
 
