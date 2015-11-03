c315c0747f099da225bd909195b126afbd6a0cba
Wildhoney <adam.timberlake@gmail.com>
2015-11-03 11:17:27 +0000

    Added additional tests

--- tests/keo.test.js
+++ tests/keo.test.js
@@ -2,17 +2,60 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import {findDOMNode} from 'react-dom';
 import Gremlin from './components/gremlin';
+import {compose, stitch} from '../src/keo';
 
 describe('Keo', () => {
 
-    it('Should be able to render and interact with the component;', () => {
+    it('Should be able to compose state and dispatch;', () => {
+
+        const mocks = {
+            setState: () => {},
+            dispatch: () => {}
+        };
+
+        spyOn(mocks, 'setState').and.callThrough();
+        spyOn(mocks, 'dispatch').and.callThrough();
+
+        const render = () => {
+
+            const drinkAndDispatch = compose(
+                state => mocks.setState(state),
+                event => mocks.dispatch(event)
+            );
+
+            return <button onClick={() => drinkAndDispatch()}>Drink Me</button>;
+
+        };
+
+        const Component = stitch({ render });
+        const instance  = TestUtils.renderIntoDocument(<Component />);
+        const button    = TestUtils.findRenderedDOMComponentWithTag(instance, 'button');
+
+        TestUtils.Simulate.click(findDOMNode(button));
+
+        expect(mocks.setState).toHaveBeenCalled();
+        expect(mocks.dispatch).toHaveBeenCalled();
+
+    });
+
+    it('Should be able to assume render function was passed in when only a function is passed to stitch;', () => {
+
+        const render    = () => <h1>Daffodil</h1>;
+        const Component = stitch(render);
+        const instance  = TestUtils.renderIntoDocument(<Component />);
+        
+        expect(TestUtils.isCompositeComponent(instance)).toBeTruthy();
+
+    });
+
+    it('Should be able to render and interact with a React component;', () => {
 
         const name     = 'Matilda';
         const instance = TestUtils.renderIntoDocument(<Gremlin name={name} />);
 
-        const h1      = TestUtils.findRenderedDOMComponentWithTag(instance, 'h1');
-        const h2      = TestUtils.findRenderedDOMComponentWithTag(instance, 'h2');
-        const buttons = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'button');
+        const h1       = TestUtils.findRenderedDOMComponentWithTag(instance, 'h1');
+        const h2       = TestUtils.findRenderedDOMComponentWithTag(instance, 'h2');
+        const buttons  = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'button');
 
         expect(findDOMNode(h1).textContent).toBe(`Señorita Zombie ${name.toUpperCase()}`);
         expect(findDOMNode(h2).textContent).toBe('Human Brain Intact: Kinda!');
