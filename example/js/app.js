bb0780fbf370c93168f2660174d27b60b7994ca7
Wildhoney <adam.timberlake@gmail.com>
2016-01-05 15:05:50 +0000

    Improved example with more realism

--- example/js/app.js
+++ example/js/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import Gremlin from '../../tests/components/Gremlin';
 
 document.addEventListener('DOMContentLoaded', () => {
