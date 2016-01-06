b6446e0bcbda08f9af17635c7d34f185e21bac80
Wildhoney <adam.timberlake@gmail.com>
2016-01-06 20:48:53 +0000

    Improved tests

--- tests/keo.test.js
+++ tests/keo.test.js
@@ -11,6 +11,7 @@ import {compose, stitch, wrap} from '../src/keo';
 import DefaultProps from './mocks/default-props';
 import StateImmediate from './mocks/state-immediate';
 import StateFuture from './mocks/state-future';
+import StateMixed from './mocks/state-mixed';
 
 // Setup JSDOM manually because we don't use Mocha.
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
@@ -32,6 +33,26 @@ test.cb('is able to set an immediate state', t => {
     component.find('button').simulate('click');
     setTimeout(() => {
         t.is(component.state().name, 'Spencer');
+        t.end();
+    });
+});
+
+test.cb('is able to set future and immediate states', t => {
+    const component = shallow(<StateFuture />);
+    component.find('button').simulate('click');
+    setTimeout(() => {
+        t.is(component.state().name, 'Spencer');
+        t.end();
+    });
+});
+
+test.cb('is able to set future and immediate states', t => {
+    const component = shallow(<StateMixed />);
+    component.find('button').simulate('click');
+    t.same(component.state(), { age: 45, friends: true });
+    setTimeout(() => {
+        const equals = { name: 'Donovan', age: 45, friends: true, visited: ['Argentina', 'Maldives', 'Brazil'] };
+        t.same(component.state(), equals);
         t.end();
     });
 });
