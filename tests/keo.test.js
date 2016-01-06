0b738176724fa23a628c6fd8c1453f33e23ed471
Wildhoney <adam.timberlake@gmail.com>
2016-01-06 11:56:59 +0000

    Using AVA for testing

--- tests/keo.test.js
+++ tests/keo.test.js
@@ -1,32 +1,41 @@
+import test from 'ava';
+import {shallow} from 'enzyme';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import {findDOMNode} from 'react-dom';
-import Gremlin from './components/gremlin';
+
+import 'babel-core/register';
 import {compose, stitch, wrap} from '../src/keo';
-import {stitch as s} from '../src/keo';
+import Gremlin from './components/gremlin';
 
-describe('Keo', () => {
+test('is able to set future and immediate values in state', t => {
 
-    it('Should be able to set state according to immediate and future values;', done => {
-
-        const instance = TestUtils.renderIntoDocument(<Gremlin name="Gemma" />);
-        expect(true).toBeTruthy();
-        done();
-
-        //const h2       = TestUtils.findRenderedDOMComponentWithTag(instance, 'h2');
-        //const h3       = TestUtils.findRenderedDOMComponentWithTag(instance, 'h3');
-        //const buttons  = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'button');
-
-        //TestUtils.Simulate.click(findDOMNode(buttons[3]));
-        //expect(instance.state.zombieName).toEqual('Reset');
-        //expect(instance.state.name instanceof Promise).toBeFalsy();
-        //
-        //setTimeout(() => {
-        //    expect(instance.state.zombieName).toEqual('Reset');
-        //    expect(instance.state.name).toEqual('No Name');
-        //    done();
-        //});
-
-    });
+    t.pass();
 
 });
+
+//describe('Keo', () => {
+//
+//    it('Should be able to set state according to immediate and future values;', done => {
+//
+//        const instance = TestUtils.renderIntoDocument(<Gremlin name="Gemma" />);
+//        expect(true).toBeTruthy();
+//        done();
+//
+//        //const h2       = TestUtils.findRenderedDOMComponentWithTag(instance, 'h2');
+//        //const h3       = TestUtils.findRenderedDOMComponentWithTag(instance, 'h3');
+//        //const buttons  = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'button');
+//
+//        //TestUtils.Simulate.click(findDOMNode(buttons[3]));
+//        //expect(instance.state.zombieName).toEqual('Reset');
+//        //expect(instance.state.name instanceof Promise).toBeFalsy();
+//        //
+//        //setTimeout(() => {
+//        //    expect(instance.state.zombieName).toEqual('Reset');
+//        //    expect(instance.state.name).toEqual('No Name');
+//        //    done();
+//        //});
+//
+//    });
+//
+//});
