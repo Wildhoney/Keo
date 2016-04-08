e516d371fbf0af5b43d74617bfc773064f7a309e
Wildhoney <adam.timberlake@gmail.com>
2016-04-08 18:18:20 +0100

    Added cleaner hash URLs

--- example/js/default.js
+++ example/js/default.js
@@ -4,7 +4,8 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 import reducers from './reducers';
 import Layout from './containers/layout';
 
@@ -14,10 +15,11 @@ const store = createStoreWithMiddleware(reducers);
 ready().then(() => {
 
     const mountNode = document.querySelector('.todo-app');
+    const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
     
     render((
         <Provider store={store}>
-            <Router history={hashHistory}>
+            <Router history={appHistory}>
                 <Route path="/" component={Layout} />
                 <Route path="/:status" component={Layout} />
             </Router>
