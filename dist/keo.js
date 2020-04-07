bc53574e9e97da790da967f42c0a355ac5b41e87
Tim Gates <tim.gates@iress.com>
2020-04-07 19:24:27 +1000

    docs: Fix simple typo, maddness -> madness
    There is a small typo in dist/keo.js.
    
    Should read `madness` rather than `maddness`.

--- dist/keo.js
+++ dist/keo.js
@@ -148,7 +148,7 @@ function runTimeout(fun) {
         return setTimeout(fun, 0);
     }
     try {
-        // when when somebody has screwed with setTimeout but no I.E. maddness
+        // when when somebody has screwed with setTimeout but no I.E. madness
         return cachedSetTimeout(fun, 0);
     } catch (e) {
         try {
@@ -171,7 +171,7 @@ function runClearTimeout(marker) {
         return clearTimeout(marker);
     }
     try {
-        // when when somebody has screwed with setTimeout but no I.E. maddness
+        // when when somebody has screwed with setTimeout but no I.E. madness
         return cachedClearTimeout(marker);
     } catch (e) {
         try {
